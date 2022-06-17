7a50068925c93404c7ff6a013788a1d803868d9b
ironsummit72 <ironsumit72@gmail.com>
2022-06-17 12:41:44 +0530

    added public and styles

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ const express= require("express");
 const ejs= require("ejs");
 const mongoose= require("mongoose");
 const path= require("path");
+const port=800;
+
+
+
+const app = express();
+app.use(express.static(path.join(__dirname,'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get("/", (req, res) => {
+    res.render('index')
+});
+
+
+app.listen(port,function() {
+    console.log(`listening on port ${port}`);
+});
+
